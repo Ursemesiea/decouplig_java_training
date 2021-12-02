fd879a3951884e0dda766fb72535ef39ba059639
Ursemesiea <ngoufackkenfac@et.esiea.fr>
2021-12-02 15:17:05 +0100

    Simplification des messages de log

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -5,8 +5,8 @@ import java.util.function.Predicate;
 public class LoggerFactory {
     public static Logger getLogger (String name)
     {
-        ConsoleLogger consoleLogger = new ConsoleLogger();
-        return consoleLogger;
+        Predicate<String> condition = message -> message.contains("simulation");
+        return new CompositeLogger(new ContextualLogger(new ConsoleLogger(),name) , new ContextualLogger(new FilteredLogger(new FileLogger("logs.txt"),condition),name));
     }
 
 
